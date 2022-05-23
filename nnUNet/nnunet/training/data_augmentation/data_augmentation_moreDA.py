dedbbfac268d4dae351cc09e11aefd1a6e1fa809
cuijun1997 <472944587@qq.com>
2022-05-23 16:46:11 +0800

    init3

---
+++ nnUNet/nnunet/training/data_augmentation/data_augmentation_moreDA.py
@@ -0,0 +1,246 @@
+#    Copyright 2020 Division of Medical Image Computing, German Cancer Research Center (DKFZ), Heidelberg, Germany
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from batchgenerators.dataloading import MultiThreadedAugmenter
+from batchgenerators.transforms import DataChannelSelectionTransform, SegChannelSelectionTransform, SpatialTransform, \
+    GammaTransform, MirrorTransform, Compose
+from batchgenerators.transforms.color_transforms import BrightnessMultiplicativeTransform, \
+    ContrastAugmentationTransform, BrightnessTransform
+from batchgenerators.transforms.noise_transforms import GaussianNoiseTransform, GaussianBlurTransform
+from batchgenerators.transforms.resample_transforms import SimulateLowResolutionTransform
+from batchgenerators.transforms.utility_transforms import RemoveLabelTransform, RenameTransform, NumpyToTensor
+from nnunet.training.data_augmentation.custom_transforms import Convert3DTo2DTransform, Convert2DTo3DTransform, \
+    MaskTransform, ConvertSegmentationToRegionsTransform
+from nnunet.training.data_augmentation.default_data_augmentation import default_3D_augmentation_params
+from nnunet.training.data_augmentation.downsampling import DownsampleSegForDSTransform3, DownsampleSegForDSTransform2
+from nnunet.training.data_augmentation.pyramid_augmentations import MoveSegAsOneHotToData, \
+    ApplyRandomBinaryOperatorTransform, \
+    RemoveRandomConnectedComponentFromOneHotEncodingTransform
+
+try:
+    from batchgenerators.dataloading.nondet_multi_threaded_augmenter import NonDetMultiThreadedAugmenter
+except ImportError as ie:
+    NonDetMultiThreadedAugmenter = None
+
+
+def get_moreDA_augmentation(dataloader_train, dataloader_val, patch_size, params=default_3D_augmentation_params,
+                            border_val_seg=-1,
+                            seeds_train=None, seeds_val=None, order_seg=1, order_data=3, deep_supervision_scales=None,
+                            soft_ds=False,
+                            classes=None, pin_memory=True, regions=None,
+                            use_nondetMultiThreadedAugmenter: bool = False):
+    assert params.get('mirror') is None, "old version of params, use new keyword do_mirror"
+
+    tr_transforms = []
+
+    if params.get("selected_data_channels") is not None:
+        tr_transforms.append(DataChannelSelectionTransform(params.get("selected_data_channels")))
+
+    if params.get("selected_seg_channels") is not None:
+        tr_transforms.append(SegChannelSelectionTransform(params.get("selected_seg_channels")))
+
+    # don't do color augmentations while in 2d mode with 3d data because the color channel is overloaded!!
+    if params.get("dummy_2D") is not None and params.get("dummy_2D"):
+        ignore_axes = (0,)
+        tr_transforms.append(Convert3DTo2DTransform())
+    else:
+        ignore_axes = None
+    # aug:more_DA增强train添加的位置
+    # 旋转(三向旋转)
+    # 缩放
+    # 随机裁剪
+    # 边缘填充
+    # 高斯噪声增强，
+    # 高斯模糊
+    # 多值亮度增强
+    # 额外的亮度增强，这个增强符合高斯分布（正态分布）
+    # 这个高斯分布的亮度增强的意思是，他的亮度增强矩阵的值分布是符合高斯分布的，具体值范围由的sigma和mu值决定
+    # 对比增强，体素强度与在（0.65，1.5）--默认值--均匀随机采样的值相乘，乘完之后把这个值裁剪到他们原始强度范围内
+    # 低像素仿真，将图像先下采样，order=0，然后再上采样order=3
+    # 这里有一个点比较有意思，额外亮度增强、对比增强和伽马增强，如果他的增强范围代表了抑制到增强，增强会使得数据一半增强一半抑制
+    # 镜像增强
+    # 如果之前做个强度归一化，进行mask处理，把-1区域消除
+    # 
+    tr_transforms.append(SpatialTransform(
+        patch_size, patch_center_dist_from_border=None,
+        do_elastic_deform=params.get("do_elastic"), alpha=params.get("elastic_deform_alpha"),
+        sigma=params.get("elastic_deform_sigma"),
+        do_rotation=params.get("do_rotation"), angle_x=params.get("rotation_x"), angle_y=params.get("rotation_y"),
+        angle_z=params.get("rotation_z"), p_rot_per_axis=params.get("rotation_p_per_axis"),
+        do_scale=params.get("do_scaling"), scale=params.get("scale_range"),
+        border_mode_data=params.get("border_mode_data"), border_cval_data=0, order_data=order_data,
+        border_mode_seg="constant", border_cval_seg=border_val_seg,
+        order_seg=order_seg, random_crop=params.get("random_crop"), p_el_per_sample=params.get("p_eldef"),
+        p_scale_per_sample=params.get("p_scale"), p_rot_per_sample=params.get("p_rot"),
+        independent_scale_for_each_axis=params.get("independent_scale_factor_for_each_axis")
+    ))
+
+    if params.get("dummy_2D"):
+        tr_transforms.append(Convert2DTo3DTransform())
+
+    # we need to put the color augmentations after the dummy 2d part (if applicable). Otherwise the overloaded color
+    # channel gets in the way
+    tr_transforms.append(GaussianNoiseTransform(p_per_sample=0.1))
+    tr_transforms.append(GaussianBlurTransform((0.5, 1.), different_sigma_per_channel=True, p_per_sample=0.2,
+                                               p_per_channel=0.5))
+    tr_transforms.append(BrightnessMultiplicativeTransform(multiplier_range=(0.75, 1.25), p_per_sample=0.15))
+
+    if params.get("do_additive_brightness"):
+        tr_transforms.append(BrightnessTransform(params.get("additive_brightness_mu"),
+                                                 params.get("additive_brightness_sigma"),
+                                                 True, p_per_sample=params.get("additive_brightness_p_per_sample"),
+                                                 p_per_channel=params.get("additive_brightness_p_per_channel")))
+
+    tr_transforms.append(ContrastAugmentationTransform(p_per_sample=0.15))
+    tr_transforms.append(SimulateLowResolutionTransform(zoom_range=(0.5, 1), per_channel=True,
+                                                        p_per_channel=0.5,
+                                                        order_downsample=0, order_upsample=3, p_per_sample=0.25,
+                                                        ignore_axes=ignore_axes))
+    tr_transforms.append(
+        GammaTransform(params.get("gamma_range"), True, True, retain_stats=params.get("gamma_retain_stats"),
+                       p_per_sample=0.1))  # inverted gamma
+
+    if params.get("do_gamma"):
+        tr_transforms.append(
+            GammaTransform(params.get("gamma_range"), False, True, retain_stats=params.get("gamma_retain_stats"),
+                           p_per_sample=params["p_gamma"]))
+
+    if params.get("do_mirror") or params.get("mirror"):
+        tr_transforms.append(MirrorTransform(params.get("mirror_axes")))
+    
+    # TAG: more_da 预处理中采用了强度归一化
+    # 他这里数据构成是一个dict，{'data':data,'seg':seg}，所以它可以选择对数据或seg处理
+    # 因为label标签里无元素区域会被置为-1
+    # 这里会把img里，label中的无元素区域从-1置为0
+    # img[mask<0]=0
+    if params.get("mask_was_used_for_normalization") is not None:
+        mask_was_used_for_normalization = params.get("mask_was_used_for_normalization")
+        tr_transforms.append(MaskTransform(mask_was_used_for_normalization, mask_idx_in_seg=0, set_outside_to=0))
+
+    #TAG:数据label还原 将-1置为0 
+    tr_transforms.append(RemoveLabelTransform(-1, 0))
+
+    # todo:级联seg标签放到最后一层?
+    # 这里的配置是给级联配置的，将最后seg结果变成onehot标签并且保存在dict中的data中
+    if params.get("move_last_seg_chanel_to_data") is not None and params.get("move_last_seg_chanel_to_data"):
+        tr_transforms.append(MoveSegAsOneHotToData(1, params.get("all_segmentation_labels"), 'seg', 'data'))
+        if params.get("cascade_do_cascade_augmentations") is not None and params.get(
+                "cascade_do_cascade_augmentations"):
+            if params.get("cascade_random_binary_transform_p") > 0:
+                tr_transforms.append(ApplyRandomBinaryOperatorTransform(
+                    channel_idx=list(range(-len(params.get("all_segmentation_labels")), 0)),
+                    p_per_sample=params.get("cascade_random_binary_transform_p"),
+                    key="data",
+                    strel_size=params.get("cascade_random_binary_transform_size"),
+                    p_per_label=params.get("cascade_random_binary_transform_p_per_label")))
+            if params.get("cascade_remove_conn_comp_p") > 0:
+                tr_transforms.append(
+                    RemoveRandomConnectedComponentFromOneHotEncodingTransform(
+                        channel_idx=list(range(-len(params.get("all_segmentation_labels")), 0)),
+                        key="data",
+                        p_per_sample=params.get("cascade_remove_conn_comp_p"),
+                        fill_with_other_class_p=params.get("cascade_remove_conn_comp_max_size_percent_threshold"),
+                        dont_do_if_covers_more_than_X_percent=params.get(
+                            "cascade_remove_conn_comp_fill_with_other_class_p")))
+
+    # 把字典中seg字段变成了target字段了，seg其实就是target嘛，这里为了统一之后的结果
+    tr_transforms.append(RenameTransform('seg', 'target', True))
+
+    if regions is not None:
+        tr_transforms.append(ConvertSegmentationToRegionsTransform(regions, 'target', 'target'))
+
+    # todo:深监督参数的意义是什么？
+    # aug:深监督将target增多
+    # soft_ds一般默认为false
+    # 这里将会把target变为降采样的多个图
+    # 这些图会按照网络结构的降采样，进行降采样操作
+    # 比如原本的降采样操作是
+
+    if deep_supervision_scales is not None:
+        if soft_ds:
+            assert classes is not None
+            tr_transforms.append(DownsampleSegForDSTransform3(deep_supervision_scales, 'target', 'target', classes))
+        else:
+            tr_transforms.append(DownsampleSegForDSTransform2(deep_supervision_scales, 0, 0, input_key='target',
+                                                              output_key='target'))
+
+    tr_transforms.append(NumpyToTensor(['data', 'target'], 'float'))
+    # 组合batchgenerater的这些操作整合
+    tr_transforms = Compose(tr_transforms)
+
+    #todo:NonDetMultiThreadedAugmentater是干什么的？  
+    if use_nondetMultiThreadedAugmenter:
+        if NonDetMultiThreadedAugmenter is None:
+            raise RuntimeError('NonDetMultiThreadedAugmenter is not yet available')
+        batchgenerator_train = NonDetMultiThreadedAugmenter(dataloader_train, tr_transforms, params.get('num_threads'),
+                                                            params.get("num_cached_per_thread"), seeds=seeds_train,
+                                                            pin_memory=pin_memory)
+    else:
+        # TAG:增强器的创建
+        # 这里用的是多线程更新，正常使用的不停动态更新
+        batchgenerator_train = MultiThreadedAugmenter(dataloader_train, tr_transforms, params.get('num_threads'),
+                                                      params.get("num_cached_per_thread"),
+                                                      seeds=seeds_train, pin_memory=pin_memory)
+    # batchgenerator_train = SingleThreadedAugmenter(dataloader_train, tr_transforms)
+    # import IPython;IPython.embed()
+
+    # AUG:more_DA增强val集
+    # 去除掉seg中的-1标签
+    # 然后将seg标签转换为target
+    # 深监督转换
+    # numpy转换为tensor
+    # val集是没有做裁剪的
+    val_transforms = []
+    val_transforms.append(RemoveLabelTransform(-1, 0))
+    if params.get("selected_data_channels") is not None:
+        val_transforms.append(DataChannelSelectionTransform(params.get("selected_data_channels")))
+    if params.get("selected_seg_channels") is not None:
+        val_transforms.append(SegChannelSelectionTransform(params.get("selected_seg_channels")))
+
+    if params.get("move_last_seg_chanel_to_data") is not None and params.get("move_last_seg_chanel_to_data"):
+        val_transforms.append(MoveSegAsOneHotToData(1, params.get("all_segmentation_labels"), 'seg', 'data'))
+
+    val_transforms.append(RenameTransform('seg', 'target', True))
+
+    # region这个参数不太懂是干啥的，默认为None
+    if regions is not None:
+        val_transforms.append(ConvertSegmentationToRegionsTransform(regions, 'target', 'target'))
+
+    if deep_supervision_scales is not None:
+        if soft_ds:
+            assert classes is not None
+            val_transforms.append(DownsampleSegForDSTransform3(deep_supervision_scales, 'target', 'target', classes))
+        else:
+            val_transforms.append(DownsampleSegForDSTransform2(deep_supervision_scales, 0, 0, input_key='target',
+                                                               output_key='target'))
+
+    val_transforms.append(NumpyToTensor(['data', 'target'], 'float'))
+    val_transforms = Compose(val_transforms)
+
+    if use_nondetMultiThreadedAugmenter:
+        if NonDetMultiThreadedAugmenter is None:
+            raise RuntimeError('NonDetMultiThreadedAugmenter is not yet available')
+        batchgenerator_val = NonDetMultiThreadedAugmenter(dataloader_val, val_transforms,
+                                                          max(params.get('num_threads') // 2, 1),
+                                                          params.get("num_cached_per_thread"),
+                                                          seeds=seeds_val, pin_memory=pin_memory)
+    else:
+        batchgenerator_val = MultiThreadedAugmenter(dataloader_val, val_transforms,
+                                                    max(params.get('num_threads') // 2, 1),
+                                                    params.get("num_cached_per_thread"),
+                                                    seeds=seeds_val, pin_memory=pin_memory)
+    # batchgenerator_val = SingleThreadedAugmenter(dataloader_val, val_transforms)
+    # 最终返回两个增强器，使用next()就可以获得需要batch数量的batch数
+    return batchgenerator_train, batchgenerator_val
+
---
+++ nnUNet/nnunet/training/loss_functions/deep_supervision.py
@@ -0,0 +1,59 @@
+#    Copyright 2020 Division of Medical Image Computing, German Cancer Research Center (DKFZ), Heidelberg, Germany
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+
+from torch import nn
+
+
+class MultipleOutputLoss2(nn.Module):
+    def __init__(self, loss, weight_factors=None):
+        """
+        use this if you have several outputs and ground truth (both list of same len) and the loss should be computed
+        between them (x[0] and y[0], x[1] and y[1] etc)
+        :param loss:原始的loss计算规则，trainv2中使用的是DC_and_CE_loss
+        :param weight_factors:
+        """
+        super(MultipleOutputLoss2, self).__init__()
+        self.weight_factors = weight_factors
+        self.loss = loss
+
+    def forward(self, x, y):
+        # x是output，y是target
+        # x是一个tuple，第一个是最后一层的结果，然后倒数第二，倒数第三
+
+        assert isinstance(x, (tuple, list)), "x must be either tuple or list"
+        assert isinstance(y, (tuple, list)), "y must be either tuple or list"
+        if self.weight_factors is None:
+            weights = [1] * len(x)
+        else:
+            weights = self.weight_factors
+
+        # l = weights[0] * self.loss(x[0], y[0])
+        # for i in range(1, len(x)):
+        #     if weights[i] != 0:
+        #         l += weights[i] * self.loss(x[i], y[i])
+        # return l
+
+        result,ce,dice = self.loss(x[0], y[0])
+        l = weights[0] * result
+        l_ce = weights[0] * ce
+        l_dice = weights[0] * dice
+        for i in range(1, len(x)):
+            if weights[i] != 0:
+                result,ce,dice = self.loss(x[i], y[i])
+                l += weights[i] * result
+                l_ce += weights[i] * ce
+                l_dice += weights[i] * dice
+       
+        return l,l_ce,l_dice
